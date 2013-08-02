c80c23280a40e2179b70f971cd2f243b4c65a822
Artur Heinze <artur@yootheme.com>
2013-08-02 14:24:43 +0200

    enable loading also external js files

--- assets.js
+++ assets.js
@@ -1,18 +1,18 @@
 ;(function($, win){
- 
+
     win.Assets = {
- 
+
         _ress: {},
- 
+
         require: function(ress, callback) {
- 
+
             var req  = [],
                 ress = $.isArray(ress) ? ress:[ress];
- 
+
             for (var i=0, len=ress.length; i<len; i++) {
- 
+
                 if(!ress[i]) continue;
- 
+
                 if (!this._ress[ress[i]]) {
                    if (ress[i].match(/\.js$/)) {
                     this._ress[ress[i]] = this.getScript(ress[i]);
@@ -22,32 +22,50 @@
                 }
                 req.push(this._ress[ress[i]]);
             }
- 
+
             return $.when.apply(null, req).done(callback);
         },
- 
+
         getScript: function(url, callback) {
-            return $.getScript(url, callback);
+
+            var d = $.Deferred(), script = document.createElement('script');
+
+            script.async = true;
+
+            script.onload = function() {
+                d.resolve();
+                if(callback) { callback(script); }
+            };
+
+            script.onerror = function() {
+                d.reject(url);
+            };
+
+            script.src = url;
+
+            document.getElementsByTagName('head')[0].appendChild(script);
+
+            return d.promise();
         },
- 
+
         getCss: function(url, callback){
             var d         = $.Deferred(),
                 link      = document.createElement('link');
                 link.type = 'text/css';
                 link.rel  = 'stylesheet';
                 link.href = url;
- 
+
             document.getElementsByTagName('head')[0].appendChild(link);
- 
+
             var img = document.createElement('img');
                 img.onerror = function(){
                     d.resolve();
                     if(callback) callback(link);
                 };
                 img.src = url;
- 
+
             return d.promise();
         }
     };
- 
+
 })(jQuery, window);
